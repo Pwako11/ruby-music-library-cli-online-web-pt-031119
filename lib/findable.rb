be2062e5b44dd183f4bf0654b2b472b10fd9e692
Patrick A <akolo@hotmail.com>
2019-04-29 02:51:14 +0000

    Automatically backed up by Learn

--- lib/findable.rb
+++ lib/findable.rb
@@ -1,6 +1,6 @@
 module Findable
   module ClassMethods
-    def self.find_by_name(name)
+    def find_by_name(name)
       self.all.detect { |i| i.name == name}
     end
     def find_or_create_by_name(name)
